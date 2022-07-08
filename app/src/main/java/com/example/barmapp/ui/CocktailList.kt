d8865cd2725bf2fb8f48a610957f0c58e3134ebb
laia <laia.igelmo.a@gmail.com>
2022-07-09 00:08:33 +0200

    - Create visualization of a list item. --> cocktail_item.xml
    - Create visualization of the cocktail list. --> cocktail_list.xml
    
    - Import a dummy image of a cocktail --> cocktail.jpeg
    
    - Add body of the CocktailList.kt and add a dummy mutable list of cocktail names. --> CocktailList.kt

--- app/src/main/java/com/example/barmapp/ui/CocktailList.kt
+++ app/src/main/java/com/example/barmapp/ui/CocktailList.kt
@@ -1,4 +1,33 @@
 package com.example.barmapp.ui
 
-class CocktailList {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import com.example.barmapp.R
+import com.example.barmapp.databinding.CocktailListBinding
+
+class CocktailList : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: CocktailListBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val recyclerView: List<String>
+        recyclerView = mutableListOf("Cocktail 1", "Cocktail 2", "Cocktail 3")
+
+        binding = CocktailListBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setSupportActionBar(binding.toolbar)
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment_content_main)
+        return navController.navigateUp(appBarConfiguration)
+                || super.onSupportNavigateUp()
+    }
 }
