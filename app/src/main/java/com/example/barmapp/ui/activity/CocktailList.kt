a6a7b619ae12439da31daf3be220da8cf5212cfd
laia <laia.igelmo.a@gmail.com>
2022-07-09 14:37:43 +0200

    - Create adapter for the CocktailList.kt recyclerview. - Redo of the CocktailList.kt activity. Created adapter attached to the CocktailList.kt activity. Create a dummy list of cocktail names in order to check the proper functioning of the recyclerview and view in general.

--- app/src/main/java/com/example/barmapp/ui/activity/CocktailList.kt
+++ app/src/main/java/com/example/barmapp/ui/activity/CocktailList.kt
@@ -1,27 +1,32 @@
-package com.example.barmapp.ui
+package com.example.barmapp.ui.activity
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.barmapp.R
 import com.example.barmapp.databinding.CocktailListBinding
+import com.example.barmapp.ui.adapter.CocktailListAdapter
 
 class CocktailList : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: CocktailListBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        val recyclerView: List<String>
-        recyclerView = mutableListOf("Cocktail 1", "Cocktail 2", "Cocktail 3")
 
         binding = CocktailListBinding.inflate(layoutInflater)
         setContentView(binding.root)
         setSupportActionBar(binding.toolbar)
+        val manager = LinearLayoutManager(this)
+        binding.listOfCocktails.layoutManager = manager
+        val listCocktails: List<String>
+        listCocktails = mutableListOf("Cocktail 1", "Cocktail 2", "Cocktail 3")
+        val cocktailListAdapter = CocktailListAdapter(listCocktails)
+
+        binding.listOfCocktails.adapter = cocktailListAdapter
 
     }
 
