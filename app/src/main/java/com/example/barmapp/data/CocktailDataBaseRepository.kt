160a4f21bc020d4b2b82097e51cff3ab5f290c75
laia <laia.igelmo.a@gmail.com>
2022-07-11 18:33:46 +0200

    - Add gradient on detail cocktail image.
    - Remove unused repository functions.

--- app/src/main/java/com/example/barmapp/data/CocktailDataBaseRepository.kt
+++ app/src/main/java/com/example/barmapp/data/CocktailDataBaseRepository.kt
@@ -9,16 +9,4 @@ import retrofit2.http.GET
 class CocktailDataBaseRepository : CocktailRepository{
     //TODO: Add with dependency injection the retrofit service later on
     override suspend fun getCocktailsStartingWithA() = CocktailDBAPI.retrofitService.getCocktailsStartingWithA().drinks
-
-    override fun getCocktailDescription(id: Int): String {
-        TODO("Not yet implemented")
-    }
-
-    override fun getCocktailIngredients(id: Int): List<String> {
-        TODO("Not yet implemented")
-    }
-
-    override fun getImage(id: Int): String {
-        TODO("Not yet implemented")
-    }
 }
--- app/src/main/java/com/example/barmapp/domain/repository/CocktailRepository.kt
+++ app/src/main/java/com/example/barmapp/domain/repository/CocktailRepository.kt
@@ -5,7 +5,4 @@ import com.example.barmapp.domain.dataStructures.Cocktail
 
 interface CocktailRepository {
     suspend fun getCocktailsStartingWithA() : List<Cocktail>
-    fun getCocktailDescription(id: Int) : String
-    fun getCocktailIngredients(id: Int) : List<String>
-    fun getImage(id: Int): String
 }
